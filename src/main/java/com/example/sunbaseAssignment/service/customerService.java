904f82b7938e3bb4c5f30961ad757fc06912021f
Niteshkarmore7888 <niteshkarmo444@gmail.com>
2024-04-05 15:11:50 +0530

    add

---
+++ src/main/java/com/example/sunbaseAssignment/service/customerService.java
@@ -0,0 +1,23 @@
+package com.example.sunbaseAssignment.service;
+
+import com.example.sunbaseAssignment.Dto.Request.CustomerRequestDto;
+import com.example.sunbaseAssignment.Dto.Response.CustomerResponseDto;
+import org.springframework.data.domain.Page;
+
+import java.util.List;
+
+public interface customerService {
+
+    CustomerResponseDto createCustomer(CustomerRequestDto customerRequestDto , boolean SyncDb);
+
+    CustomerResponseDto udapteCustomer(String email , CustomerRequestDto customerRequestDto);
+
+
+    String deleteCustomer(String email);
+
+    CustomerResponseDto getCustomerWithId(String email);
+
+    List<CustomerResponseDto> searchBySpecificType(String searchBy, String searchQuery);
+
+    Page<CustomerResponseDto> getAllCustomers(int pageNo, int rowsCount, String sortBy, String searchBy);
+}
